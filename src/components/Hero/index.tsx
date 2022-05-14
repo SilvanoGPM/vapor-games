371a4a9562db2cd8996d7652242e1813bf4dcbc1
SkyG0D <silvanosilvino@hotmail.com>
2022-05-14 03:05:34 -0300

    feat: Hero component improved

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,10 +1,23 @@
-import { Box, Flex, Heading, Tag, TagLeftIcon, Text } from '@chakra-ui/react';
-import { AddIcon } from '@chakra-ui/icons';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Tag,
+  TagLeftIcon,
+  Text,
+} from '@chakra-ui/react';
 
-const bgImage =
-  'https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg';
+import { AddIcon, ArrowRightIcon } from '@chakra-ui/icons';
 
-export function Hero() {
+type HeroProps = {
+  title: string;
+  publisher: string;
+  bgImage: string;
+  genres: string[];
+};
+
+export function Hero({ title, publisher, bgImage, genres }: HeroProps) {
   return (
     <Box
       as="section"
@@ -18,30 +31,39 @@ export function Hero() {
       <Box
         w="100%"
         h="100%"
+        boxShadow="dark-lg"
         px={{ base: '2rem', lg: '4rem' }}
         color="white"
-        paddingTop={{ base: '100px', md: '300px', lg: '350px' }}
-        bg="linear-gradient(to bottom left, transparent 10%, rgba(0, 0, 0, 0.95) 80%)"
+        paddingTop={{ base: '50px', md: '300px', lg: '350px' }}
+        bgGradient="linear(to bottom left, transparent 10%, rgba(0, 0, 0, 0.95) 80%)"
       >
-        <Heading mb="1rem">Grand Theft Auto V</Heading>
-        <Text mb="1rem">Rockstar Games</Text>
+        <Heading mb="1rem">{title}</Heading>
+        <Text mb="1rem">{publisher}</Text>
 
-        <Flex gap={['0.1rem', '0.5rem']} wrap="wrap">
-          <Tag colorScheme="green">
-            <TagLeftIcon boxSize="12px" as={AddIcon} />
-            Action
-          </Tag>
-
-          <Tag colorScheme="teal">
-            <TagLeftIcon boxSize="12px" as={AddIcon} />
-            Adventure
-          </Tag>
-
-          <Tag colorScheme="orange">
-            <TagLeftIcon boxSize="12px" as={AddIcon} />
-            Singleplayer
-          </Tag>
+        <Flex gap={['0.1rem', '0.5rem']} wrap="wrap" mb="1rem" width="100%">
+          {genres.map((genre) => (
+            <Tag key={genre} colorScheme="whiteAlpha">
+              <TagLeftIcon boxSize="12px" as={AddIcon} />
+              {genre}
+            </Tag>
+          ))}
         </Flex>
+
+        <Button
+          colorScheme="whiteAlpha"
+          rightIcon={<ArrowRightIcon />}
+          width="100%"
+          maxW="400px"
+          _focus={{
+            outline: 'none',
+            ringColor: 'whiteAlpha.800',
+            ring: 1,
+            ringOffsetColor: 'rgba(0, 0, 0, 0.95)',
+            ringOffset: '0.2rem',
+          }}
+        >
+          Detalhes
+        </Button>
       </Box>
     </Box>
   );
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,12 @@
 import { Hero } from 'components/Hero';
 
 export function Main() {
-  return <Hero />;
+  return (
+    <Hero
+      title="Grand Theft Auto V"
+      publisher="Rockstar Games"
+      bgImage="https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg"
+      genres={['Action', 'Adventure', 'Singleplayer']}
+    />
+  );
 }
